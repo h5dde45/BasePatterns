2c2cfa7cbcb33f7072999c737ba0d2b6b3c94e3c
tmvf <tmvf@yandex.ru>
2017-07-24 15:55:30 +0300

    singleton

---
+++ src/com/basepatterns/creation/singleton/ProgramRunner.java
@@ -0,0 +1,12 @@
+package com.basepatterns.creation.singleton;
+
+public class ProgramRunner {
+    public static void main(String[] args) {
+
+        ProgramLogger.getProgramLogger().addLogInfo("1");
+        ProgramLogger.getProgramLogger().addLogInfo("2");
+        ProgramLogger.getProgramLogger().addLogInfo("3");
+
+        ProgramLogger.getProgramLogger().showLogFile();
+    }
+}
