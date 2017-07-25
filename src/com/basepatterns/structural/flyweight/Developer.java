ea2a7ac7047f5caa2ec2431773b74bf79d0f2a20
tmvf <tmvf@yandex.ru>
2017-07-25 18:04:54 +0300

    Flyweight

---
+++ src/com/basepatterns/structural/flyweight/Developer.java
@@ -0,0 +1,5 @@
+package com.basepatterns.structural.flyweight;
+
+public interface Developer {
+    void writeCode();
+}
---
+++ src/com/basepatterns/structural/flyweight/DeveloperFactory.java
@@ -0,0 +1,26 @@
+package com.basepatterns.structural.flyweight;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class DeveloperFactory {
+    private static final Map<String, Developer> developers = new HashMap<>();
+
+    public Developer getDeveloperBySpecialty(String specialty) {
+        Developer developer = developers.get(specialty);
+        if (developer == null) {
+            switch (specialty) {
+                case "java":
+                    System.out.println("Hiring java developer");
+                    developer = new JavaDeveloper();
+                    break;
+                case "c++":
+                    System.out.println("Hiring c++ developer");
+                    developer = new CppDeveloper();
+                    break;
+            }
+            developers.put(specialty,developer);
+        }
+        return developer;
+    }
+}
